5bf6c0ed3bdc8aee859d2b298f53cfede001202a
Andrew West <apwest@gmail.com>
2017-11-05 20:28:41 -0500

    Build Board using two loops

--- src/index.js
+++ src/index.js
@@ -27,21 +27,11 @@ class Board extends React.Component {
   render() {
     return (
       <div>
+      {[0,1,2].map((row) => (
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {[0,1,2].map((col) => this.renderSquare(3*row + col))}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+      ))}
       </div>
     );
   }
